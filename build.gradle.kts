a63d4ff34f0fff777fa88143ccaa19e283d49e04
lynas <szlynas@gmail.com>
2022-10-29 17:32:56 +0200

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.5"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.lynas"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.github.resilience4j:resilience4j-spring-boot2:1.7.1")
+	implementation("org.springframework.boot:spring-boot-starter-actuator") // required in classpath for resilience4j
+	implementation("org.springframework.boot:spring-boot-starter-aop") // required in classpath for resilience4j
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/lynas/SpringRetryResilience4jApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lynas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringRetryResilience4jApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
